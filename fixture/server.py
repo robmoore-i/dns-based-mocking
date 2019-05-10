7e4b23fbf30c85baa3c3d55a5a74cac62608e996
romo <robert.moore@zuhlke.com>
2019-05-10 16:08:50 +0100

    now also searches google

--- fixture/server.py
+++ fixture/server.py
@@ -14,17 +14,24 @@ class RequestHandler(BaseHTTPRequestHandler):
         response = requests.get(url)
         css_classes = list(
             map(lambda s: s[7:], list(filter(lambda s: s.startswith("class="), response.text.split(" ")))))
-        return str(css_classes)
+        return str(css_classes[random.randint(0, len(css_classes))])
 
     def random_string(self):
         return "".join(list(map(lambda i: "abcdefghij"[int(i)], str(random.randint(100000, 1000000)))))
 
+    def search_google(self, query):
+        url = "https://www.google.com/search?client=safari&rls=en&q=" + query + "&ie=UTF-8&oe=UTF-8"
+        response = requests.get(url)
+        random_position = random.randint(0, len(response.text) - 20)
+        return response.text[random_position:random_position + 10]
+
     def do_GET(self):
-        message = self.search_github(self.random_string())
+        oracle_prediction = self.search_google(self.search_github(self.random_string()))
+        text_content = "The oracle has seen your future: \"" + oracle_prediction + "\""
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.write(message.encode('utf-8'))
+        self.wfile.write(text_content.encode('utf-8'))
 
 
 class Server:
