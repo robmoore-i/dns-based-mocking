ed769700a8864d50cd2c30869dbf841fdf6f109e
romo <robert.moore@zuhlke.com>
2019-05-10 15:59:04 +0100

    Make it query github

--- fixture/server.py
+++ fixture/server.py
@@ -1,4 +1,6 @@
 import sys
+import random
+import requests
 from http.server import *
 
 if len(sys.argv) != 2:
@@ -7,11 +9,22 @@ if len(sys.argv) != 2:
 
 
 class RequestHandler(BaseHTTPRequestHandler):
+    def search_github(self, query):
+        url = "https://github.com/search?q=" + query
+        response = requests.get(url)
+        css_classes = list(
+            map(lambda s: s[7:], list(filter(lambda s: s.startswith("class="), response.text.split(" ")))))
+        return str(css_classes)
+
+    def random_string(self):
+        return "".join(list(map(lambda i: "abcdefghij"[int(i)], str(random.randint(100000, 1000000)))))
+
     def do_GET(self):
+        message = self.search_github(self.random_string())
         self.send_response(200)
         self.send_header('Content-Type', 'text/plain; charset=utf-8')
         self.end_headers()
-        self.wfile.write("Hello from python3".encode('utf-8'))
+        self.wfile.write(message.encode('utf-8'))
 
 
 class Server:
